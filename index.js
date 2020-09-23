6f5908e1f64af695c77145f5c85f30bf5ad53f07
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-09-22 23:28:41 -0300

    [views & css] Created the views folder and the css in a public folder where express allows using.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const productRoutes = require("./routes/productRoutes");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(productRoutes);
+
+app.use(express.static(path.join("public")));
+
+app.use("/add-file", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "add-file.html"));
+})
+
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "shop.html"));
+})
 
 app.listen(3000, () => console.log("Porta 3000 funcionando..."));
